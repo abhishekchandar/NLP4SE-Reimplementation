e269cee040afbee1936bd1cd0164f4a6118ab1d4
Abhishek Chandar <abhishek.ctos@gmail.com>
2021-03-08 23:22:33 +0530

    Fetched pages for each category using scraper

--- build_corpus.py
+++ build_corpus.py
@@ -1,18 +1,46 @@
 # import required modules
 import wikipedia
+import pandas as pd
+import requests_cache
+import traceback
+import os
+import glob
+
 from bs4 import BeautifulSoup
 import requests
 
-print(wikipedia.page("Android (robot)").content)
-# get URL
-# page = requests.get("https://en.wikipedia.org/wiki/Main_Page")
-# # scrape webpage
-# soup = BeautifulSoup(page.content, 'html.parser')
-# list(soup.children)
-# # find all occurance of p in HTML
-# # includes HTML tags
-# print(soup.find_all('p'))
-# print('\n\n')
-# # return only text
-# # does not include HTML tags
-# print(soup.find_all('p')[0].get_text())
+df = pd.DataFrame(columns=['Text'])
+
+# got to data dir
+# for each csv file, open using pandas and loop title column
+# Replace _ with space
+# Create new df file for each csv file with all pages
+# export in each case
+path = 'C://Users//abhis//Desktop//Reimplement-NLP4SE-1//data'
+os.chdir(path)
+requests_cache.install_cache('data')
+for file in list(glob.glob('*.csv')):
+    try:
+        dataframe = pd.read_csv(file)
+    # dataframe = pd.read_csv(
+    #     'C://Users//abhis//Desktop//Reimplement-NLP4SE-1//data/ae.csv')
+        for id in dataframe['pageid']:
+            data = requests.get(
+                'https://en.wikipedia.org/w/api.php?action=query&prop=info&pageids='+str(id)+'&inprop=url&format=json')
+            data = data.json()
+            page = data['query']['pages'][str(id)]['title']
+            text = wikipedia.page(page).content
+            df = df.append({'Text': text}, ignore_index=True)
+    except Exception as e:
+        traceback.print_exc()
+
+    finally:
+        print(file+' processing done.')
+        print(df.shape)
+        df.to_csv(file+'_1.csv')
+# print(data)
+# dataframe['title'] = dataframe['title'].map(lambda x: x.replace("_", " "))
+# print(dataframe['title'])
+
+
+# print(df['Text'])
